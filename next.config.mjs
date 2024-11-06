a3ceda7a8676640ebf1521de7ba29605a300c777
Soldierplayz6867 <soldierplayz.tw@gmail.com>
2024-11-06 17:11:59 +0800

    Fix?

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 import { createMDX } from 'fumadocs-mdx/next';
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 const withMDX = createMDX();
 
@@ -65,5 +66,11 @@ const config = {
     ]
   }
 };
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
+
+export const runtime = "edge";
 
 export default withMDX(config);
